84a634cb0d1b030894b6c8f655f287077985a03d
David Muradyan <i22.muradyan.david@etud.ufar.am>
2025-04-02 17:11:41 +0400

    Add project files.

---
+++ ProjectS4API.Data/DAO/MainDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectS4API.Data.Entities;
+
+namespace ProjectS4API.Data.DAO {
+    public class MainDbContext : DbContext {
+        public MainDbContext(DbContextOptions options) : base(options) { }
+
+        // Here write down your collection. For example:
+        // If we have StudentEntity, write the following:
+        // public DbSet<StudentEntity> Students { get; set; }
+
+        public DbSet<ExampleEntity> Examples { get; set; }
+    }
+}
