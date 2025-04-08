84986bb6e5068e66759f6ed304220cae6573b6d6
David Muradyan <i22.muradyan.david@etud.ufar.am>
2025-04-08 22:40:09 +0400

    Topic-controller

---
+++ ProjectS4API/Controllers/TopicController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectS4API.Core.CRUDServices.TopicServices;
+
+namespace ProjectS4API.Controllers {
+    public class TopicController : Controller {
+        private ITopicCRUDService topicService;
+        public TopicController(ITopicCRUDService topicService) {
+            this.topicService = topicService;
+        }
+
+        [HttpGet("/CreateTopic")]
+        public IActionResult CreateTopic(CreateTopicDto dto) {
+            return Ok(topicService.Create(dto));
+        }
+
+        [HttpGet("/GetTopic")]
+
+        public IActionResult ReadTopic(int id) {
+            return Ok(topicService.Read(id));
+        }
+
+        [HttpGet("/GetAllTopics")]
+        public IActionResult ReadAllTopics() {
+            return Ok(topicService.ReadAll());
+        }
+
+        [HttpPut("/UpdateTopic")]
+        public IActionResult UpdateTopic(UpdateTopicDto dto) {
+            return Ok(topicService.Update(dto));
+        }
+
+
+        [HttpDelete("/DeleteTopic")]
+        public IActionResult DeleteTopic(int id) {
+            return Ok(topicService.Delete(id));
+        }
+    }   
+}
